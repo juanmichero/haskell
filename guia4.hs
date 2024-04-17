816fc151406dab0028b043be7a7060c30fa80ade
Juan Michero <juanmichero2002@gmail.com>
2024-04-17 00:41:00 -0300

    guia4+

--- guia4.hs
+++ guia4.hs
@@ -18,3 +18,34 @@ parteEntera x | x >= 0 && x < 1 = 0
               | x >= 1 = 1 + parteEntera (x - 1)
               | x <= 0 && x > -1 = 0
               | x <= -1 = 1 + parteEntera (x + 1)
+
+-- | x < 1 && x >= 0 = 0
+-- | x >= 1 = 1 + parteEntera (x - 1)
+-- | otherwise = (-1) + parteEntera (x + 1)
+
+--3
+
+esDivisible :: Integer -> Integer -> Bool
+esDivisible _ 0 = error ""
+esDivisible x y | x < y = False
+                | x == y = True
+                | otherwise = esDivisible (x - y) y
+
+--4
+
+sumaImpares :: Integer -> Integer
+sumaImpares n | n == 0 = 0
+              | n == 1 = 1 
+              | n > 1 = (2*n-1) + sumaImpares (n - 1)
+
+--5
+
+medioFact :: Integer -> Integer
+medioFact n | n == 0 = 1
+            | n == 1 = 1
+            | n > 1 = n * medioFact (n - 2)
+
+--6
+
+sumaDigitos :: Integer -> Integer
+sumaDigitos n
