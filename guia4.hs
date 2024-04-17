e240881ca6c8cc59422876069438c20db190f49d
Juan Michero <juanmichero2002@gmail.com>
2024-04-17 11:18:39 -0300

    guia4++

--- guia4.hs
+++ guia4.hs
@@ -26,7 +26,7 @@ parteEntera x | x >= 0 && x < 1 = 0
 --3
 
 esDivisible :: Integer -> Integer -> Bool
-esDivisible _ 0 = error ""
+-- esDivisible _ 0 = error ""
 esDivisible x y | x < y = False
                 | x == y = True
                 | otherwise = esDivisible (x - y) y
@@ -34,9 +34,9 @@ esDivisible x y | x < y = False
 --4
 
 sumaImpares :: Integer -> Integer
-sumaImpares n | n == 0 = 0
-              | n == 1 = 1 
-              | n > 1 = (2*n-1) + sumaImpares (n - 1)
+sumaImpares 0 = 0
+sumaImpares 1 = 1
+sumaImpares n | n > 1 = (2*n-1) + sumaImpares (n-1)
 
 --5
 
@@ -48,4 +48,22 @@ medioFact n | n == 0 = 1
 --6
 
 sumaDigitos :: Integer -> Integer
-sumaDigitos n
+sumaDigitos 0 = 0
+sumaDigitos n = mod n 10 + sumaDigitos (div n 10)
+
+--7
+
+-- todosDigitosIguales :: Integer -> Bool
+-- todosDigitosIguales n | mod n 10 == n = True
+                      
+
+--13
+
+sumatoriaDesdeM :: Int -> Int -> Int
+sumatoriaDesdeM i 0 = 0
+-- sumatoriaDesdeM i 1 = i es una obviedad
+sumatoriaDesdeM i m = i^m + sumatoriaDesdeM i (m-1)
+
+sumatoriaDoble :: Int -> Int -> Int
+sumatoriaDoble 0 _ = 0
+sumatoriaDoble n m = sumatoriaDesdeM n m + sumatoriaDoble (n-1) m
